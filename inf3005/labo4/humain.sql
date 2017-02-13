ae9b65503cc1c79fd0ab5e6c46a5022ffdabfb19
Lambert <lambertdeblois@gmail.com>
2017-02-13 10:06:21 -0500

    update

---
+++ inf3005/labo4/humain.sql
@@ -0,0 +1,13 @@
+create table Personne (
+  nom varchar(25) primary key,
+  prenom varchar(25),
+  age integer
+);
+
+insert into Personne values ('jackson', 'micheal', 20);
+insert into Personne values ('buble', 'micheal', 25);
+insert into Personne values ('presley', 'elvis', 30);
+insert into Personne values ('georg', 'solti', 35);
+insert into Personne values ('qincy', 'jones', 40);
+insert into Personne values ('alison', 'krauss', 50);
+insert into Personne values ('kanye', 'west', 55);
