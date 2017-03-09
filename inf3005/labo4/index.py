39a3400f62be9ef67d89504ddf8b58b676013a34
Lambert <lambertdeblois@gmail.com>
2017-03-09 15:32:47 -0500

    labs

--- inf3005/labo4/index.py
+++ inf3005/labo4/index.py
@@ -1,5 +1,5 @@
-#coding: utf8
-#labo4
+# coding: utf8
+# labo4
 
 from flask import Flask
 from flask import render_template
@@ -8,9 +8,11 @@ from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def formulaire():
     return render_template('formulaire.html')
+
 
 @app.route('/envoyer', methods=['POST'])
 def donnees_formulaire():
@@ -27,10 +29,12 @@ def donnees_formulaire():
         return redirect('/merci')
 
     print name, fname
-    return render_template('formulaire.html', errname=errname, errfname=errfname, name=name, fname=fname)
+    return render_template('formulaire.html',
+                            errname=errname, errfname=errfname,
+                            name=name, fname=fname)
 
 
-#faire classe personnes avec nom/prenom/age
+# faire classe personnes avec nom/prenom/age
 @app.route('/liste')
 def liste():
     cursor = conn.cursor()
@@ -40,7 +44,6 @@ def liste():
     for row in cursor:
         p = Personne(row[1], row[2], [3])
         personnes.append(p)
-
 
 
 @app.route('/merci')
