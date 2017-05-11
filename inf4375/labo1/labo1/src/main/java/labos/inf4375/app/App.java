e4880461f064b34a02ef9fccb86328352253f38f
Lambert <lambertdeblois@gmail.com>
2017-05-11 15:58:06 -0400

    labo11 inf4375

---
+++ inf4375/labo1/labo1/src/main/java/labos/inf4375/app/App.java
@@ -0,0 +1,13 @@
+package labos.inf4375.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
