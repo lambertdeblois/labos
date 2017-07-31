b8d5739076e4cde631f5f1864acc132f736c5f87
lambertdeblois <lambertdeblois@users.noreply.github.com>
2017-07-31 11:34:52 -0400

    Update courriel.py

--- inf3005/labo6/courriel.py
+++ inf3005/labo6/courriel.py
@@ -32,7 +32,7 @@ msg.attach(MIMEText(body, 'plain'))
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login(source_address, "secret123")
+server.login(source_address, mdp)
 text = msg.as_string()
 server.sendmail(source_address, destination_address, text)
 server.quit()
